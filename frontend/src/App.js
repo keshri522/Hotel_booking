fcbd309a7dc1c552e9da0054fa5cd2eca32799a0
keshri522 <rkeshri522@gmail.com>
2023-10-19 21:19:33 +0530

    added navbar using bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import Paths from "./Components/Routers/Route";
+import Nav from "./Components/Navbar/Nav";
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Paths />
     </div>
   );
---
+++ frontend/src/Components/Navbar/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Nav = () => {
+  return (
+    <nav className="nav bg-light d-flex justify-content-between">
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+      <Link className="nav-link" to="/Login">
+        Login
+      </Link>
+      <Link className="nav-link" to="/Register">
+        Register
+      </Link>
+    </nav>
+  );
+};
+
+export default Nav;
