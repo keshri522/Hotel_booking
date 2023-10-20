7e6991d735f2e03b9ec3c7048c02ae144d5524dc
keshri522 <rkeshri522@gmail.com>
2023-10-20 14:00:52 +0530

    added controllers

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,8 +1,9 @@
+// in this folder of auth i am adding all the authorization like login and signup
 const express = require("express");
 // simply use router to handle all the routes
 const router = express.Router();
-router.get("/:message", (req, res) => {
-  res.send(req.params.message);
-});
+// impoting the controllers
+const First = require("../controllers/first");
+router.get("/:message", First);
 
 module.exports = router;
