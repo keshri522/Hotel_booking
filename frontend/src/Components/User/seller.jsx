c7beb80efd135829f6cc0a6820d2394350f31537
keshri522 <rkeshri522@gmail.com>
2023-10-25 13:59:06 +0530

    added notConnected layouts with stripe

--- frontend/src/Components/User/seller.jsx
+++ frontend/src/Components/User/seller.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import DashboardNav from "../dashboardNav";
 import ConnectNav from "../ConnectNav";
 import { Link } from "react-router-dom";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Seller = () => {
   const navigate = useNavigate();
   // need to check this is protected routes only login user can access this routes
@@ -20,6 +22,8 @@ const Seller = () => {
   if (!(User && User.token)) {
     return null;
   }
+  // this is the onclick event
+  const ProcessPayouts = () => {};
   // based on the stipe connect showing the seller dashboard
   // if user is present or if stripe_connect is there then only show add hotels
   // otherwise show first to connect wihth stipe
@@ -47,17 +51,29 @@ const Seller = () => {
     <div className="container-fluid">
       {/* need to show a button click on that show the hotels */}
       <div className="row">
-        <div className="col-md-10">
-          <h1>Not connected with stipe</h1>
-        </div>
-        <div className="col-md-2">
-          {/* <Link
-            style={{ fontWeight: "bold", color: "white" }}
-            to="/hotels/new"
-            className="btn btn-secondary"
-          >
-            + Add Hotels
-          </Link> */}
+        <div className="col-md-6 offset-md-3  text-center">
+          <div className="p-5 pointer">
+            <FontAwesomeIcon
+              className="h1"
+              fade
+              icon={faHouse}
+            ></FontAwesomeIcon>
+            <h4>Setup payouts to post a hotel</h4>
+            <p className="lead text-primary">
+              Thrid Party Parterns to transfer earning to bank accounts
+            </p>
+            <button className="btn btn-primary" onClick={ProcessPayouts}>
+              Process Payout
+            </button>
+            <br />
+            <br />
+            <p className="text-muted">
+              <small>
+                You'll be redirected to Stripe to complete the onboarding
+                process
+              </small>
+            </p>
+          </div>
         </div>
       </div>
     </div>
