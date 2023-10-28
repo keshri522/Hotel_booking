a70f7d552c5e1cf66a500a0f23e022d16fd26cbd
keshri522 <rkeshri522@gmail.com>
2023-10-28 16:16:44 +0530

    added bootstap card to show hotels

---
+++ frontend/src/Components/Cards/bootstapCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const BootstapCard = ({ hotel }) => {
+  //   console.log(hotel);
+  return <div>{JSON.stringify(hotel)}</div>;
+};
+
+export default BootstapCard;
--- frontend/src/Components/booking/Home.jsx
+++ frontend/src/Components/booking/Home.jsx
@@ -3,21 +3,43 @@ import { useDispatch } from "react-redux";
 import { loggedInUser } from "../Redux/reducers/LoginUser";
 import { useSelector } from "react-redux";
 import Gethotels from "../Functions/GetHotles";
+import BootstapCard from "../Cards/bootstapCard";
+import Spinner from "./../Spinner/spinner";
 const Home = () => {
   const [hotels, Sethotles] = useState([]);
+  const [show, Setshow] = useState(true); // Set show to true initially
   const dispatch = useDispatch();
 
-  // calling this function in useeffect
   useEffect(() => {
     Gethotels()
       .then((res) => {
         Sethotles(res.data);
+        Setshow(false); // Set show to false after data is fetched
       })
       .catch((err) => {
         console.log(err);
+        Setshow(false); // Set show to false in case of error
       });
   }, []);
-  return <div>{JSON.stringify(hotels)}</div>;
+
+  return (
+    <>
+      <div className="container-fluid  text-center mt-1 p-3 bg-secondary">
+        <h2 className="text-white">All Hotels</h2>
+      </div>
+      <div className="container">
+        {show ? (
+          <div className="load">
+            <h3 className="text-danger">...loading</h3>
+          </div>
+        ) : (
+          hotels.map((hotel) => (
+            <BootstapCard key={hotel._id} hotel={hotel}></BootstapCard>
+          ))
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Home;
